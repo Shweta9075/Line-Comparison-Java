26a457b733ec946d6bfe24366b2fe8aa8ce8857e
shweta <shwetabirajdar5872gmail.com>
2022-09-12 09:00:03 +0530

    Added :Ccalculate length

---
+++ src/com/bridgelabz/Linecomparison.java
@@ -0,0 +1,18 @@
+package com.bridgelabz;
+
+public class Linecomparison {
+    public static void main(String[] args) {
+        System.out.println("Welcome");
+        int x1 = 1;
+        int y1 = 1;
+        int x2 = 4;
+        int y2 = 4;
+        double length;
+        length = Math.sqrt((x2-x1) *2 + (y2-y1) * 2);
+        System.out.println("Length of the Co-ordinates is : " + length);
+
+
+
+
+    }
+}
